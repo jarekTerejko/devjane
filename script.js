68c0e6a619cf26c23d06986ef1a08e6f5e096067
Jarosław Terejko <j.terejko@gmail.com>
2020-02-02 02:57:13 +0100

    added contact form and validation

--- script.js
+++ script.js
@@ -1,25 +1,85 @@
-const navBtn = document.querySelector(".nav-toggle")
-const nav = document.querySelector(".nav")
-const navLinks = document.querySelectorAll('.nav__link')
+const navBtn = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav__link");
 
-navBtn.addEventListener("click", ()=> {
-    if(nav.classList.contains('open')) {
-        nav.classList.remove('open')
-        navBtn.classList.remove("open")
-        nav.classList.add("closed")
-    } else {
-        nav.classList.remove('closed')
-        navBtn.classList.add("open")
-        nav.classList.add("open")
+navBtn.addEventListener("click", () => {
+  if (nav.classList.contains("open")) {
+    nav.classList.remove("open");
+    navBtn.classList.remove("open");
+    nav.classList.add("closed");
+  } else {
+    nav.classList.remove("closed");
+    navBtn.classList.add("open");
+    nav.classList.add("open");
+  }
+});
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    if (nav.classList.contains("open")) {
+      nav.classList.remove("open");
+      navBtn.classList.remove("open");
     }
-    
-})
+  });
+});
 
-navLinks.forEach(link=> {
-    link.addEventListener('click', ()=> {
-        if(nav.classList.contains('open')) {
-            nav.classList.remove("open")
-            navBtn.classList.remove('open')
-        }
-    })
-})
+// form
+
+const form = document.querySelector(".contact-form");
+const userName = document.querySelector(".contact-form__name");
+const mail = document.querySelector(".contact-form__mail");
+const message = document.querySelector(".contact-form__message");
+
+form.addEventListener("submit", e => {
+  checkInputs();
+  const contactsRows = document.querySelectorAll(".contact-form__row");
+  contactsRows.forEach(row => {
+    if (row.classList.contains("error")) {
+      e.preventDefault();
+    }
+  });
+});
+
+const checkInputs = () => {
+  const userNameValue = userName.value.trim();
+  const emailValue = mail.value.trim();
+  const messageValue = message.value.trim();
+
+  if (userNameValue === "") {
+    setErrorFor(userName, "Name cannot be blank...");
+  } else {
+    setSuccessFor(userName);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(mail, "Email cannot be blank...");
+  } else if (!isMailValid(emailValue)) {
+    setErrorFor(email, "Please provide a valid email");
+  } else {
+    setSuccessFor(mail);
+  }
+
+  if (messageValue === "") {
+    setErrorFor(message, "Message cannot be blank...");
+  } else {
+    setSuccessFor(message);
+  }
+};
+
+const setErrorFor = (input, msg) => {
+  const inputContainer = input.parentElement;
+  const errorContainer = inputContainer.querySelector(".contact-form__info");
+  inputContainer.className = "contact-form__row error";
+  errorContainer.textContent = msg;
+};
+
+setSuccessFor = input => {
+  const inputContainer = input.parentElement;
+  inputContainer.className = "contact-form__row success";
+};
+
+const isMailValid = email => {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+};
